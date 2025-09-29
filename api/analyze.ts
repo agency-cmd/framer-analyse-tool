89f0ca04b285addd0501264c6d8b3b8ff762fc25
agency-cmd <agency@luqy.studio>
2025-09-29 19:56:51 +0200

    Update analyze.ts

--- api/analyze.ts
+++ api/analyze.ts
@@ -43,6 +43,7 @@ export default async function handler(
 
     const rateLimitKey = `rate-limit:${userIp}`;
     const userRequests = (await kv.get<number>(rateLimitKey)) || 0;
+console.info("userRequests:", userRequests); 
     if (userRequests >= 2) {
         //return res.status(429).json({ limitReached: true, message: 'Tageslimit von 2 Analysen erreicht.' });
     }
